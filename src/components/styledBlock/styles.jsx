426a867cb7a0b76a303f613ecebdbb33f408fc89
ihorbar1 <ihorbar1@gmail.com>
2020-05-08 01:43:03 +0300

    history test

--- src/components/styledBlock/styles.jsx
+++ src/components/styledBlock/styles.jsx
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const MainBlock = styled.div`
    width: 500px;
-   height: 500px;
+   height: 600px;
    background-color:${props => props.valid ? (props.test ? '#000' : '#555') : '#765'}; 
 `
